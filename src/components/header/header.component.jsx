07a9dd664f2302219837b67e621ce488494ab56a
W Lawless <wlawless.sharpe@protonmail.com>
2021-10-27 16:38:20 -0400

    cart redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,7 +9,8 @@ import { ReactComponent as Logo } from '../../assets/crown.svg'
 import CartIcon from '../cart-icon/cart-icon.component'
 import CartMenu from '../cart-menu/cart-menu.component'
 
-const Header = ({currentUser}) => {
+const Header = ({currentUser, hidden}) => {
+    console.log(hidden)
     return (
     <div className="header">
         <Link className='logo-container' to="/">
@@ -26,13 +27,17 @@ const Header = ({currentUser}) => {
             }
             <CartIcon />
         </div>
-        <CartMenu />
+        {
+            hidden ? null : <CartMenu />
+        }
     </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+
+const mapStateToProps = ({user: { currentUser}, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header)
